0ff2f78d28245735772e049e72a689aaf989d810
3p10 <soleymani.3p10@gmail.com>
2024-11-19 13:12:00 +0100

    prints the MD5 hash of each line from a file

--- Programming/code.py
+++ Programming/code.py
@@ -1,16 +1,16 @@
 import hashlib, sys, os
 
 def md5(input):
-    return hashlib.md5(input.encode()).hexdigest()
+    return hashlib.md5(input.encode()).hexdigest() # Function to compute MD5
 
-file_name = sys.argv[1]
+file_name = sys.argv[1]  # Get the file name
 
 if os.path.exists(file_name):
     with open(file_name, 'r') as f:
-        content = f.read()
+        content = f.read() # Read the file's content
 else:
     print("The file does not exist!")
     sys.exit()
 
-for f in content.split('\n'):
+for f in content.split('\n'): # Loop through each line of the file
     print(md5(f))
