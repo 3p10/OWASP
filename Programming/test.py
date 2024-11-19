3401ce91bf420761ac931bf6333d4ab3f52e98d8
3p10 <soleymani.3p10@gmail.com>
2024-11-19 17:50:33 +0100

    Extracts and prints the 'data-nonce' value from a webpage

--- Programming/test.py
+++ Programming/test.py
@@ -3,9 +3,9 @@ import requests, re
 r = requests.session()
 res = r.get('https://memoryleaks.ir/wp-login.php')
 
-match = re.search(r'data-nonce="(\w+)"', res.text)
+match = re.search(r'data-nonce="(\w+)"', res.text) # Search for the 'data-nonce' value in the response HTML
 
 if match:
-    print("Found nonce:", match.group(1))
+    print("Found nonce:", match.group(1)) # Print the extracted nonce value
 else:
     print("Nonce not found")
